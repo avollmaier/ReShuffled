d0d4a7112e8a52c01cc358b4a700e68b8175cec2
“volalm15” <alois.vollm@gmail.com>
2019-05-21 20:32:52 +0200

    general commit of master module

--- Scripts/PushAll.sh
+++ Scripts/PushAll.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-
-progress-bar() {
-  local duration=${1}
-
-
-    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
-    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
-    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
-    clean_line() { printf "\r"; }
-
-  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
-      already_done; remaining; percentage
-      sleep 1
-      clean_line
-  done
-  clean_line
-}
-
-clear
-progress-bar 12
 echo "Updating..."
+
 cd ..
 cd Reshuffled_Dokumentation/
 git checkout master
@@ -27,6 +8,7 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule dokumentation"
 git push origin master
+
 cd ..
 cd Reshuffled_Elektronik/
 git checkout master
@@ -34,6 +16,7 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule elektronik"
 git push origin master
+
 cd ..
 cd Reshuffled_Informatik/
 git checkout master
@@ -41,6 +24,7 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule informatik"
 git push origin master
+
 cd ..
 cd Reshuffled_Mechanik/
 git checkout master
@@ -48,12 +32,14 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule mechanik"
 git push origin master
+
 cd ..
 git checkout master
 git pull origin master
 git add .
 git commit -m "general commit of master module"
 git push origin master
+
 clear
 echo "Done"
 
