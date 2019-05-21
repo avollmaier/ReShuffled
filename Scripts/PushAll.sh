29442a5360ee77cda7bee3eff3ebc60a9ae0a6dc
“volalm15” <alois.vollm@gmail.com>
2019-05-21 20:26:22 +0200

    general commit of master module

--- Scripts/PushAll.sh
+++ Scripts/PushAll.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Updating..."
-
 cd ..
 cd Reshuffled_Dokumentation/
 git checkout master
@@ -8,7 +7,6 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule dokumentation"
 git push origin master
-
 cd ..
 cd Reshuffled_Elektronik/
 git checkout master
@@ -16,7 +14,6 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule elektronik"
 git push origin master
-
 cd ..
 cd Reshuffled_Informatik/
 git checkout master
@@ -24,7 +21,6 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule informatik"
 git push origin master
-
 cd ..
 cd Reshuffled_Mechanik/
 git checkout master
@@ -32,16 +28,20 @@ git pull origin master
 git add .
 git commit -m "general commit of submodule mechanik"
 git push origin master
-
 cd ..
-
 git checkout master
 git pull origin master
 git add .
 git commit -m "general commit of master module"
 git push origin master
-
 clear
 echo "Done"
+
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 
 
