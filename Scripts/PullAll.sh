cd2d6deb65e57fa7fb3f5db9b4b61d089c2a0a61
“volalm15” <alois.vollm@gmail.com>
2019-05-22 09:42:04 +0200

    general commit of master module

--- Scripts/PullAll.sh
+++ Scripts/PullAll.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 echo "Updating..."
-cd /
-cd home/alois/Schreibtisch/Drive/
-cd Reshuffled_Diplomarbeitsarchiv/
+cd ..
 git pull --recurse-submodules
 git submodule update --remote
-
 git pull origin master
 clear
 echo "Done"
