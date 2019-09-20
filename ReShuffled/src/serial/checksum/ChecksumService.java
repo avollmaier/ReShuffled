a7f50ac554edf95c46196fde5bf4a815c7261d4f
volalm15 <alois.vollm@gmail.com>
2019-09-20 22:21:03 +0200

    Add CRC check and calculation

--- ReShuffled/src/serial/checksum/ChecksumService.java
+++ ReShuffled/src/serial/checksum/ChecksumService.java
@@ -6,6 +6,8 @@
 package serial.checksum;
 
 
+import logging.Logger;
+
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
@@ -13,12 +15,31 @@ import java.util.zip.Checksum;
  * @author alois
  */
 public class ChecksumService {
+    private static final Logger LOG = Logger.getLogger(ChecksumService.class.getName());
+
     public static long calcCRC(String input) {
         byte[] bytes = input.getBytes();
         Checksum checksum = new CRC32();
 
-
         checksum.update(bytes, 0, bytes.length);
         return checksum.getValue();
+    }
+
+    public static boolean checkCRC(String receivedChecksumString) {
+        String[] checkData = null;
+        try {
+            receivedChecksumString = receivedChecksumString.replaceAll("^.", "");
+            receivedChecksumString = receivedChecksumString.replaceAll("..$", "");
+            checkData = receivedChecksumString.split("#");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            LOG.severe(ex);
+        }
+        if (calcCRC(checkData[0]) == Long.parseLong(checkData[1])) return true;
+        else return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkCRC(":XX#1443568741/n"));
     }
 }
