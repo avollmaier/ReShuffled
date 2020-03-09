2632a37ad5989bf90b8a7cc9bbeaf74422466e4d
volalm15 <alois.vollm@gmail.com>
2020-03-09 13:54:22 +0100

    added abstract

---
+++ UMLDiagrams/Class Diagrams/serial/Communication.java
@@ -0,0 +1,183 @@
+package serial;
+
+import exception.SerialException;
+import data.config.Config;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.LinkedList;
+import logging.Logger;
+import serial.request.Request;
+import serial.request.Response;
+
+
+/**
+ *
+ * @author volalm15
+ */
+public class Communication {
+
+    private static final Logger LOG = Logger.getLogger(Communication.class.getName());
+    private static Communication instance;
+
+
+    public static Communication createInstance (Serial serial) {
+        if (instance != null) {
+            throw new IllegalStateException("instance already created");
+        }
+        instance = new Communication(serial);
+        return instance;
+    }
+
+
+    public static Communication getInstance () {
+        if (instance == null) {
+            throw new IllegalStateException("instance not created yet");
+        }
+        return instance;
+    }
+
+    // *********************************************************
+    private final Serial serial;
+    private final Thread communicationSendThread;
+    private final Thread communicationReceiveThread;
+    private final LinkedList<Request> toSentList = new LinkedList<>();
+    private final LinkedList<Response> responseList = new LinkedList<>();
+    private Request pendingRequest;
+
+    private final long timeoutMillis = Config.getInstance().getConfigSerial().getTimeoutMillis();
+    private final int maxReceiveFrameLength = Config.getInstance().getConfigSerial().getMaxReceiveFrameLength();
+
+
+    @SuppressWarnings("CallToThreadStartDuringObjectConstruction")
+    private Communication (Serial serial) {
+        this.serial = serial;
+        communicationReceiveThread = new Thread(new CommReceiveThread());
+        communicationSendThread = new Thread(new CommSendThread());
+        communicationReceiveThread.start();
+        communicationSendThread.start();
+    }
+
+
+    public void shutdown () {
+        communicationSendThread.interrupt();
+        communicationReceiveThread.interrupt();
+    }
+
+
+    public void sendRequestExecutor (Request req) {
+        synchronized (toSentList) {
+            toSentList.add(req);
+            toSentList.notifyAll();
+        }
+    }
+
+
+    private class CommReceiveThread implements Runnable {
+
+        @Override
+        public void run () {
+            Thread.currentThread().setName("Communication Receive Thread");
+            LOG.info(Thread.currentThread().getName() + " started");
+
+            try {
+                final InputStream is = Serial.getInstance().getInputStream();
+                final byte buffer[] = new byte[maxReceiveFrameLength];
+                int bufferIndex = 0;
+                while (!Thread.currentThread().isInterrupted()) {
+                    int b = is.read();
+                    if (b >= 0 && b < 255) {
+                        if (bufferIndex == 0) {
+                            if (b == ':') {
+                                buffer[bufferIndex++] = (byte) b;
+                            }
+                            else {
+                                LOG.warning("receiving unexpected byte (':' expected, get " + b);
+                            }
+
+                        }
+                        else if (bufferIndex < buffer.length) {
+                            buffer[bufferIndex++] = (byte) b;
+                            if (b == '\n') {
+                                synchronized (responseList) {
+                                    byte[] resBuffer = Arrays.copyOf(buffer, bufferIndex);
+                                    bufferIndex = 0;
+                                    responseList.add(new Response(resBuffer));
+                                    responseList.notifyAll();
+                                }
+                            }
+
+                        }
+                        else {
+                            LOG.warning("receiving byte " + b + " cannot be stored (receive buffer overflow)");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) {
+                LOG.warning(ex, Thread.currentThread().getName() + " exception");
+                ex.printStackTrace(System.err);
+            }
+            finally {
+                LOG.info(Thread.currentThread().getName() + " ended");
+            }
+        }
+
+    }
+
+
+    private class CommSendThread implements Runnable {
+
+        @Override
+        public void run () {
+            Thread.currentThread().setName("Communication Send Thread");
+            LOG.info(Thread.currentThread().getName() + " started");
+
+            InputStream is = Serial.getInstance().getInputStream();
+
+            try {
+                mainLoop:
+                while (!Thread.currentThread().isInterrupted()) {
+                    synchronized (toSentList) {
+                        LOG.fine("Thread waiting for items ...");
+                        toSentList.wait();
+                        if (!toSentList.isEmpty()) {
+                            pendingRequest = toSentList.removeFirst();
+                            LOG.fine("sending request " + pendingRequest.getMreqFrame());
+                            serial.writeString(pendingRequest.getMreqFrame());
+                            pendingRequest.handleRequestSent();
+
+                            Response res = null;
+                            synchronized (responseList) {
+                                responseList.wait(timeoutMillis);
+                                if (responseList.isEmpty()) {
+                                    if (Config.getInstance().getConfigSerial().isSecondTryAllowed()) {
+                                        sendRequestExecutor(pendingRequest);//send second response
+                                        continue mainLoop;
+                                    }
+                                    throw new SerialException("request timeout, mssing response"); // if second try isnt allowed throw serial exception
+                                }
+                                res = responseList.removeFirst();
+                                if (!responseList.isEmpty()) {
+                                    LOG.warning("response list should be empty, but " + responseList.size() + " items in list");
+                                    responseList.clear();
+                                }
+                            }
+                            pendingRequest.handleResponse(res);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex) {
+                LOG.warning(ex, Thread.currentThread().getName() + " exception");
+                ex.printStackTrace(System.err);
+            }
+            finally {
+                LOG.info(Thread.currentThread().getName() + " ended");
+            }
+
+        }
+
+    }
+
+}
---
+++ UMLDiagrams/Class Diagrams/serial/request/Request.java
@@ -0,0 +1,159 @@
+package serial.request;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import logging.Logger;
+import exception.SerialException;
+
+/**
+ *
+ * @author volalm15
+ */
+public abstract class Request {
+
+    private static final Logger LOG = Logger.getLogger(Request.class.getName());
+
+    private static final java.util.zip.CRC32 CRC32 = new java.util.zip.CRC32();
+    private static final DateFormat DATAFORMATTER = new SimpleDateFormat("mm:ss.S");
+
+    private String reqFrame;              // frame bytes sent to µC (only text)
+    private final long timeMillisCreatedAt;     // epoch time when this object is created
+    private long timeMillisFrameSent;     // epoch time when frame is sent to µC
+    private byte[] resFrame;             // frame bytes received from µC
+    private long timeMillisFrameReceived; // epoch time ehne frame from µC is received  
+
+    public Request() {
+        timeMillisCreatedAt = System.currentTimeMillis();
+    }
+
+    public void handleRequestSent() throws IOException {
+        timeMillisFrameSent = System.currentTimeMillis();
+
+    }
+
+    public void handleResponse(Response res) throws SerialException {
+        timeMillisFrameReceived = res.getTimeMillisCreatedAt();
+        byte[] receivedContentCRC = new byte[2];
+        String receivedCRC = "", receivedContent ="";
+        CRC32.reset();
+        resFrame = res.getResFrame();
+        LOG.fine("Received response " + Arrays.toString(resFrame));
+
+        System.arraycopy(resFrame, 1, receivedContentCRC, 0, 2);
+        CRC32.update(receivedContentCRC);
+
+        for (int i = 4; i <= 11; i++) {
+            receivedCRC = receivedCRC + (char) resFrame[i];
+        }
+
+        if (receivedCRC.equals(String.format("%08X", CRC32.getValue()))) {
+            LOG.fine("Correct CRC");
+        } else {
+            LOG.fine("Wrong CRC");
+            throw new SerialException("Wrong CRC");
+        }
+        
+        for (int i = 1; i <= 2; i++) {
+            receivedContent = receivedContent + (char) resFrame[i];
+        }
+        
+        switch (receivedContent) {
+            case "OK": {
+                LOG.fine("Response OK on board");
+            
+            } break;
+            case "E1": {
+                LOG.severe("Error 1 on board");
+            
+            } break;
+            case "E2": {
+                LOG.severe("Error 2 on board");
+            
+            } break;
+            case "E3": {
+                LOG.severe("Error 3 on board");
+            
+            } break;
+            
+            default: {
+                LOG.warning("Received unmatchable response data");
+            }
+        }
+
+    }
+
+    protected void createRequestFrame(String content) {
+
+        if (content == null || content.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        CRC32.reset();
+        CRC32.update(content.getBytes());
+        final String crc32 = String.format("%08X", CRC32.getValue());
+        final StringBuilder sb = new StringBuilder(128);
+        sb.append(':').append(content).append('#').append(crc32).append('\n');
+        reqFrame = sb.toString();
+    }
+
+    public String getMreqFrame() {
+        return reqFrame;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(128);
+        sb.append("Request").append('{');
+        sb.append("created=").append(DATAFORMATTER.format(timeMillisCreatedAt));
+
+        sb.append(", mreqFrame='");
+        for (int i = 0; i < reqFrame.length(); i++) {
+            final char c = reqFrame.charAt(i);
+            if (c >= ' ' && c <= '~') {
+                sb.append(c);
+            } else if (c == '\n') {
+                sb.append("\\n");
+            } else {
+                sb.append("\\").append(Character.getNumericValue(c));
+            }
+        }
+        sb.append("'");
+        if (timeMillisFrameSent > 0) {
+            sb.append(", sentTime=+").append((timeMillisFrameSent - timeMillisCreatedAt)).append("ms");
+        }
+        if (timeMillisFrameReceived > 0) {
+            sb.append(", sentTime=+").append((timeMillisFrameReceived - timeMillisCreatedAt)).append("ms");
+        }
+
+        if (resFrame != null) {
+            sb.append(", mresFrame='");
+            for (byte b : resFrame) {
+                if (b >= 32 && b < 127) {
+                    sb.append((char) b);
+                } else if (b == '\n') {
+                    sb.append("\\n");
+                } else {
+                    sb.append("\\").append(b < 0 ? (int) b + 256 : (int) b);
+                }
+            }
+        }
+        sb.append('}');
+
+        return sb.toString();
+    }
+
+    public long getTimeMillisCreatedAt() {
+        return timeMillisCreatedAt;
+    }
+
+    public long getTimeMillisFrameSent() {
+        return timeMillisFrameSent;
+    }
+
+    public long getTimeMillisFrameReceived() {
+        return timeMillisFrameReceived;
+    }
+
+}
